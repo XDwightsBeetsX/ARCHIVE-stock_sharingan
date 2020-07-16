9d3150fbdb9d098b33449926b2897a66cd8fbc16
John Gutierrez <johng2000@tamu.edu>
2020-07-15 23:14:31 -0500

    better way to get path

--- src/tickerplotter.py
+++ src/tickerplotter.py
@@ -4,27 +4,27 @@ import pandas as pd
 import os
 import re
 
-av_key_source_path = "H:/dev/"
+api_key_source_path = "H:\\"
+api_key_filename = "alphavantagekey.txt"
 
 
-def get_av_key(av_key_path):
-    av_key_filename = "alphavantagekey.txt"
-    av_key = ""
-
-    for root, dirs, files in os.walk(av_key_path):
-        if av_key_filename in files:
-            with open(av_key_path + av_key_filename, "r") as file:
+def get_api_key(api_key_path):
+    api_key = ""
+    for root, dirs, files in os.walk(api_key_path):
+        if api_key_filename in files:
+            with open(os.path.join(root, api_key_filename), "r") as file:
                 try:
-                    av_key = str(re.split("\n", file.read())[0])
+                    api_key = str(re.split("\n", file.read())[0])
                 except TypeError:
                     print("ERROR {file does not contain a key}")
                     quit()
-    if av_key == "":
+    if api_key == "":
         print("ERROR {file not found}")
         quit()
     else:
-        return av_key
+        return api_key
 
 
 if __name__ == "__main__":
-    print("found key:", get_av_key(av_key_source_path))
+    key = get_api_key(api_key_source_path)
+    print(f"found api key: {key}")
