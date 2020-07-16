4f300b78cc1c6c8b236ea13be1ad151694774e2b
John Gutierrez <johng2000@tamu.edu>
2020-07-15 23:00:23 -0500

    trash code finds key in text in base dir

--- src/tickerplotter.py
+++ src/tickerplotter.py
@@ -1,17 +1,30 @@
 import alpha_vantage as av
 import numpy as np
 import pandas as pd
+import os
+import re
+
+av_key_source_path = "H:/dev/"
 
 
-class StockInfo:
-    ticker = ""
-    price = 0.00
+def get_av_key(av_key_path):
+    av_key_filename = "alphavantagekey.txt"
+    av_key = ""
 
-    def __init__(self, ticker, price):
-        self.ticker = ticker
-        self.price = price
+    for root, dirs, files in os.walk(av_key_path):
+        if av_key_filename in files:
+            with open(av_key_path + av_key_filename, "r") as file:
+                try:
+                    av_key = str(re.split("\n", file.read())[0])
+                except TypeError:
+                    print("ERROR {file does not contain a key}")
+                    quit()
+    if av_key == "":
+        print("ERROR {file not found}")
+        quit()
+    else:
+        return av_key
 
 
 if __name__ == "__main__":
-    apple = StockInfo("aapl", 120.71)
-    print(apple.ticker, apple.price)
+    print("found key:", get_av_key(av_key_source_path))
