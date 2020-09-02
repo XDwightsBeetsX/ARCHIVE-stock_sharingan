02548dfe3e9d8c26d38f6c77881e4926020685ff
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2020-09-01 22:51:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-print("Running stock-sharingan...")
+"""
+Stock Sharinagan
+
+Steps to activate your sharingan:
+    1. Get an API key
+    2. Save the key to a .txt file
+    3. Set up the api_path variable
+    4. Use the Sharingan
+"""
 
 if __name__ == "__main__":
+    print("Running stock-sharingan...")
+
     print("ok")
+    # api_call_frequency = 5
+
+    # some things to consider:
+    #   how to implement requests
+    #       terminal commands
+    #       setup.txt parsing
+    #       (get email)
 
 else:
     print("error finding the project")
