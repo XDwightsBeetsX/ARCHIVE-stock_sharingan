2114b6a147ef2d858c1d3beca0da03509dbfda54
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2020-09-02 11:11:42 -0500

    validation of key works

--- main.py
+++ main.py
@@ -10,24 +10,19 @@ Steps to activate your sharingan:
 """
 import src.Api as Api
 
-api_path = "C:\\dev\\"
-api_filename = "iex_key.txt"
+api_path = "C:\\dev"
+api_filename = "iex_keyasdf.txt"
 
 if __name__ == "__main__":
     print("[SS] Running stock-sharingan...")
     try:
-        print("[SS]-[API] Looking for API key in " + api_path + api_filename)
+        print("[SS]-[API] Looking for API key in " + api_path + "\\" + api_filename)
         api_key = Api.get_api(api_path, api_filename)
-    except RuntimeError:
-        print("[SS]-[API] Could not locate/validate your key.")
-        print("[SS]-[API] Make sure the only contents of the file is your key.")
-        print("[SS]-[API] Path checked: " + api_path + api_filename)
+    except Exception:
+        print("[SS]-[API]-[ERROR] Could not locate/validate your key.")
+        print("[SS]-[API]-[ERROR] Make sure the only contents of the file is your key.")
+        print("[SS]-[API]-[ERROR] Path checked: " + api_path + api_filename)
 
-    # some things to consider:
-    #   how to implement requests
-    #       terminal commands
-    #       setup.txt parsing
-    #       (get email)
 
 else:
-    print("error finding the project")
+    print("[SS]-[SYS]-[ERROR] Error running the project")
--- src/Api.py
+++ src/Api.py
@@ -6,9 +6,16 @@ This module handles any api aspects such as:
     all calls???
 """
 import os
+import pandas as pd
+from iexfinance.refdata import get_symbols
 
 
 def get_api(api_path, api_filename):
+    """
+    Searches through directories and files of api_path for api_filename and returns the api_key
+    api_key is run through verify_api_key to ensure validity
+    if fails, throws error caught in main
+    """
     for root, dirs, files in os.walk(api_path):
         for file in files:
             if file.endswith(api_filename):
@@ -16,19 +23,30 @@ def get_api(api_path, api_filename):
                 try:
                     with open(os.path.join(root, file), "r") as api_file:
                         api_key = api_file.read().splitlines()[0]
-                        if verify_api_key(api_key):
-                            print("[SS]-[API] Found valid key:", api_key)
+                        if verify_api_key(api_key, root):
+                            print("[SS]-[API] Key verified:", api_key)
                             return api_key
                         else:
-                            print("[SS]-[API] Key was found, but is not valid.")
+                            print("[SS]-[API]-[ERROR] Key was found, but is not valid.")
                             return ""
-                except e:
-                    print("[SS]-[API] Encountered error while searching for key: " + e)
+                except Exception:
+                    print("[SS]-[API]-[ERROR] Encountered error while reading key.")
                     return ""
-    raise RuntimeError
+    raise Exception
 
 
-def verify_api_key(api_key):
-    # make call w key and see if error thrown
-
+def verify_api_key(api_key, api_key_path):
+    """
+    Used by get_api to ensure the key is valid
+    Does this by attempting a call to get stock_references
+    """
+    try:
+        stock_references = get_symbols(output_format='pandas', token=api_key)
+    except Exception:
+        return False
+    writer = pd.ExcelWriter(api_key_path + "\\stock_references_iexcloud.xlsx")
+    stock_references.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    writer.close()
+    print("[SS]-[API] Wrote stock references to api filepath.")
     return True
