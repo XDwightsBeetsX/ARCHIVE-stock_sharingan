ba42d094a007530d51513e09138c20b978a9f034
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2020-09-14 15:43:23 -0500

    ^

--- src/Time.py
+++ src/Time.py
@@ -6,15 +6,44 @@ This module handles retrieving
 import time as t
 import datetime as dt
 
-def get_last_workday(ref_date=dt.datetime.today()):
-    timedelta = dt.timedelta(max(1, (ref_date.weekday() + 6) % 7 - 3))
-    last_workday = ref_date - timedelta
+def get_prev_workday(ref_date=dt.datetime.today()):
+    """
+    Returns the last workday.
+    If after 5p friday, returns the current day.
+    """
+    prev_workday = ref_date - dt.timedelta(max(1, (ref_date.day + 6) % 7 - 3))
+    prev_workday = prev_workday.replace(hour=0, minute=0, second=0, microsecond=0)
     
-    return last_workday
+    if ref_date.day == 4 and ref_date > dt.datetime.combine(ref_date, dt.time(hour=17)):
+        prev_workday += dt.timedelta(days=1)
+    
+    return prev_workday
 
 
-def get_last_workweek(ref_date=dt.datetime.today()):
-    last_monday = ref_date + dt.timedelta(-ref_date.weekday(), weeks=-1)
-    last_friday = ref_date + dt.timedelta(-ref_date.weekday() - 3)
+def get_prev_workweek(ref_date=dt.datetime.today()):
+    """
+    Returns the last monday and friday.
+    If after 5p friday, returns the current week.
+    """
+    week = dt.timedelta(days=0, weeks=1)
+    # clear time data, and go back a week
+    prev_monday = ref_date - dt.timedelta(days=ref_date.weekday(),
+        hours=ref_date.hour,
+        minutes=ref_date.minute,
+        seconds=ref_date.second,
+        microseconds=ref_date.microsecond) - week
+    prev_friday = prev_monday + dt.timedelta(days=4)
 
-    return last_monday, last_friday
+    # if a weekend, update to wk that just ended
+    if ref_date.day > 4:
+        prev_monday += week
+        prev_friday += week
+
+    # if after 5 on a friday, prev_monday is this monday
+    elif ref_date.day == 4 and ref_date - dt.datetime.combine(prev_friday, dt.time(17)) > week:
+        prev_monday += week
+        prev_friday += week
+    
+    return prev_monday, prev_friday
+
+print(get_prev_workday(dt.datetime(2020, 9, 4, 17, 1)))
--- tests/test_time.py
+++ tests/test_time.py
@@ -2,9 +2,54 @@ import pytest
 import datetime as dt
 import src.Time as Time
 
-def test_get_last_workday():
-    testDay1 = dt.date(2020, 9, 6)  # Sunday
-    last_workday_test = Time.get_last_workday(testDay1)
-    last_workday1 = dt.date(2020, 9, 4)
-    test1 = last_workday1 == last_workday_test
-    assert test1
+def test_get_prev_workday_1():
+    testDay = dt.datetime(2020, 9, 6)  # Sunday
+    prev_workday_test = Time.get_prev_workday(testDay)
+    prev_workday = dt.datetime(2020, 9, 4)  # Friday
+    test = prev_workday == prev_workday_test
+    assert test
+
+def test_get_prev_workday_2():
+    testDay = dt.datetime(2020, 9, 7)  # Monday
+    prev_workday_test = Time.get_prev_workday(testDay)
+    prev_workday = dt.datetime(2020, 9, 4)  # Friday (same)
+    test = prev_workday == prev_workday_test
+    assert test
+
+def test_get_prev_workday_3():
+    testDay = dt.datetime(2020, 9, 9)  # Wednesday
+    prev_workday_test = Time.get_prev_workday(testDay)
+    prev_workday = dt.datetime(2020, 9, 8)  # Tuesday
+    test = prev_workday == prev_workday_test
+    assert test
+
+def test_get_prev_workday_4():
+    testDay = dt.datetime(2020, 9, 4, 17, 1)  # Friday 1min after 5
+    prev_workday_test = Time.get_prev_workday(testDay)
+    prev_workday = dt.datetime(2020, 9, 4)  # Friday (same)
+    test = prev_workday == prev_workday_test
+    assert test
+
+def test_get_prev_workwk_1():
+    testDay = dt.datetime(2020, 9, 6, 0, 0)  # Sunday
+    prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
+    prev_wk_m_test = dt.datetime(2020, 8, 31)  # Monday
+    prev_wk_f_test = dt.datetime(2020, 9, 4)  # Friday
+    test = (prev_wk_m == prev_wk_m_test) and (prev_wk_f == prev_wk_f_test)
+    assert test
+
+def test_get_prev_workwk_2():
+    testDay = dt.datetime(2020, 9, 4, 0, 0)  # Friday
+    prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
+    prev_wk_m_test = dt.datetime(2020, 8, 24)  # Monday
+    prev_wk_f_test = dt.datetime(2020, 8, 28)  # Friday (prev)
+    test = (prev_wk_m == prev_wk_m_test) and (prev_wk_f == prev_wk_f_test)
+    assert test
+
+def test_get_prev_workwk_3():
+    testDay = dt.datetime(2020, 9, 4, 17, 1)  # Friday 1min after 5
+    prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
+    prev_wk_m_test = dt.datetime(2020, 8, 31)  # Monday
+    prev_wk_f_test = dt.datetime(2020, 9, 4)  # Friday (today)
+    test = (prev_wk_m == prev_wk_m_test) and (prev_wk_f == prev_wk_f_test)
+    assert test
