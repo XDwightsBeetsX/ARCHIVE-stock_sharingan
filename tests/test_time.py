889156c8e15c3b504cc2f7fc6111c9314be4f07e
XDwightsBeetsX <55027279+XDwightsBeetsX@users.noreply.github.com>
2020-09-14 15:46:04 -0500

    testing notes

--- tests/test_time.py
+++ tests/test_time.py
@@ -3,6 +3,7 @@ import datetime as dt
 import src.Time as Time
 
 def test_get_prev_workday_1():
+    """Sunday -> Friday"""
     testDay = dt.datetime(2020, 9, 6)  # Sunday
     prev_workday_test = Time.get_prev_workday(testDay)
     prev_workday = dt.datetime(2020, 9, 4)  # Friday
@@ -10,6 +11,7 @@ def test_get_prev_workday_1():
     assert test
 
 def test_get_prev_workday_2():
+    """Monday -> Friday"""
     testDay = dt.datetime(2020, 9, 7)  # Monday
     prev_workday_test = Time.get_prev_workday(testDay)
     prev_workday = dt.datetime(2020, 9, 4)  # Friday (same)
@@ -17,6 +19,7 @@ def test_get_prev_workday_2():
     assert test
 
 def test_get_prev_workday_3():
+    """Wednesday -> Tuesday"""
     testDay = dt.datetime(2020, 9, 9)  # Wednesday
     prev_workday_test = Time.get_prev_workday(testDay)
     prev_workday = dt.datetime(2020, 9, 8)  # Tuesday
@@ -24,6 +27,7 @@ def test_get_prev_workday_3():
     assert test
 
 def test_get_prev_workday_4():
+    """Friday 5p+ -> Friday"""
     testDay = dt.datetime(2020, 9, 4, 17, 1)  # Friday 1min after 5
     prev_workday_test = Time.get_prev_workday(testDay)
     prev_workday = dt.datetime(2020, 9, 4)  # Friday (same)
@@ -31,6 +35,7 @@ def test_get_prev_workday_4():
     assert test
 
 def test_get_prev_workwk_1():
+    """Sunday -> M/F"""
     testDay = dt.datetime(2020, 9, 6, 0, 0)  # Sunday
     prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
     prev_wk_m_test = dt.datetime(2020, 8, 31)  # Monday
@@ -39,6 +44,7 @@ def test_get_prev_workwk_1():
     assert test
 
 def test_get_prev_workwk_2():
+    """Friday -> M/F(prev)"""
     testDay = dt.datetime(2020, 9, 4, 0, 0)  # Friday
     prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
     prev_wk_m_test = dt.datetime(2020, 8, 24)  # Monday
@@ -47,9 +53,10 @@ def test_get_prev_workwk_2():
     assert test
 
 def test_get_prev_workwk_3():
+    """Friday 5p+ -> M/F(same)"""
     testDay = dt.datetime(2020, 9, 4, 17, 1)  # Friday 1min after 5
     prev_wk_m, prev_wk_f = Time.get_prev_workweek(testDay)
     prev_wk_m_test = dt.datetime(2020, 8, 31)  # Monday
-    prev_wk_f_test = dt.datetime(2020, 9, 4)  # Friday (today)
+    prev_wk_f_test = dt.datetime(2020, 9, 4)  # Friday (same)
     test = (prev_wk_m == prev_wk_m_test) and (prev_wk_f == prev_wk_f_test)
     assert test
